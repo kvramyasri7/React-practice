e9fcaf97b071407712faa2af8a47eff648fd0e6a
sowmyadev-kota <kvsowmyasri21@gmail.com>
2024-02-03 11:23:05 -0800

    feat: change color of text using react

--- App.js
+++ App.js
@@ -1,3 +1,3 @@
-const heading = React.createElement("h1",{},"Hello world form HTML");
+const heading = React.createElement("h1",{id:"heading"},"Hello world form HTML");
     const root = ReactDOM.createRoot(document.getElementById("root"));
     root.render(heading)
