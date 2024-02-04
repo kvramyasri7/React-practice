66a8b653705601f7c95f303f6ef80a9d09cd23eb
sowmyadev-kota <kvsowmyasri21@gmail.com>
2024-02-03 18:33:10 -0800

    chore: added jsx code

--- App.js
+++ App.js
@@ -1,7 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
+const jsxHeading = <h1>Hello world form HTML/JSX</h1>
 
-const heading = React.createElement("h1",{id:"heading"},"Hello world form HTML");
+
+// const heading = React.createElement("h1",{id:"heading"},"Hello world form HTML");
     const root = ReactDOM.createRoot(document.getElementById("root"));
-    root.render(heading)
+    root.render(jsxHeading)
